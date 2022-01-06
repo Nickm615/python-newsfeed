5822b3a7bef64f92d67082002ef80f428ff4f9fd
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2022-01-06 10:03:26 -0600

    Updated user table with pw hashing and email validation

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
+# Generates salt to hash passwords against
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +11,17 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+  # We use the validates decorator to create a new 'validate email' method. This method checks that the entered value contains '@' and then returns the value of the email column. The validates decorator does the rest.
+  @validates('email')
+  def validate_email(self, key, email):
+    assert '@' in email
+    # The assert keyword throws an error if the given entry does not contain the given character.
+    return email
+
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+  
