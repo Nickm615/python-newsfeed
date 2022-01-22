1db47b90f4e5ab164409b36fb33fcd17d251b359
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2022-01-22 14:32:17 -0600

    updated routes and templates for post creation and editing

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,11 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 # we use url_prefix to add /dashboard to the start of each of our routes inside this file
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
+
 @bp.route('/')
 def dash():
-  return render_template('dashboard.html')
+    db = get_db()
+    #to break up this lengthy query we must wrap it in parens to avoid a python indentation error
+    posts = (
+        db.query(Post)
+        .filter(Post.user_id == session.get('user_id'))
+        .order_by(Post.created_at.desc())
+        .all()
+    )
+    return render_template(
+      'dashboard.html',
+      posts=posts,
+      loggedIn=session.get('loggedIn')
+      
+      )
+
 
 @bp.route('/edit/<id>')
 def edit(id):
-  return render_template('edit-post.html')
+  # get single post by id
+  db = get_db()
+  post = db.query(Post).filter(Post.id == id).one()
+
+  # render edit page
+  return render_template(
+    'edit-post.html',
+    post=post,
+    loggedIn=session.get('loggedIn')
+  )
